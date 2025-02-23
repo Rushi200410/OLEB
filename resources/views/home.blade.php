f6454345be29c936b60b7a188e45f5821614d975
Rushi200410 <128353574+Rushi200410@users.noreply.github.com>
2025-02-24 00:16:50 +0530

    home page implemented

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Swaraj: Flames of Rebellion</title>
-    <link rel="stylesheet" href="styles.css">
+    <!-- <link rel="preload" as="image" href="image.png"> -->
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 <body>
-    <div class="container">
+    <div class="container scale-up-top">
         <h1>Swaraj: Flames of Rebellion</h1>
         <div class="buttons">
             <button>New Game</button>
